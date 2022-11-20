75a33de5f81f4c97511a3ee3fe301f8f72721e86
Steffi Wong <111824867+mimilame02@users.noreply.github.com>
2022-11-20 23:28:41 +0800

    Update fetch.php

--- crud-main/crud/functions/fetch.php
+++ crud-main/crud/functions/fetch.php
@@ -1,6 +1,6 @@
 <?php 
     //fetching the  data
-    $query = "SELECT * FROM tblsubjects";
+    $query = "SELECT * FROM `tblsubjects` LEFT JOIN tblsyllabus ON tblsyllabus.subject_id=tblsyllabus.subject_id";
     $result = mysqli_query($dbc, $query);
 
 ?>
