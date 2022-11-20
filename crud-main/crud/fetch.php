9b170c999b27ee0b94fbdf9988e7850db3d20aea
Steffi Wong <111824867+mimilame02@users.noreply.github.com>
2022-11-20 23:24:23 +0800

    Delete fetch.php

--- crud-main/crud/fetch.php
+++
@@ -1,6 +0,0 @@
-<?php 
-    //fetching the  data
-    $query = "SELECT * FROM `tblsubjects` LEFT JOIN tblsyllabus ON tblsyllabus.subject_id=tblsyllabus.subject_id";
-    $result = mysqli_query($dbc, $query);
-
-?>
